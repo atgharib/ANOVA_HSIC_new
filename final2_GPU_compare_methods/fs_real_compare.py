482ebc7c69d36db017ce62189fe40c40d0b4b0de
atgharib <atiye_gharib@yahoo.com>
2025-01-30 00:59:28 +0100

    compare file updated

--- final2_GPU_compare_methods/fs_real_compare.py
+++ final2_GPU_compare_methods/fs_real_compare.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
 
     dataset_names = ["breast_cancer", "sonar", "nomao", "breast_cancer_wisconsin", "skillcraft", "ionosphere", \
                      "sml", "pol",'parkinson', 'keggdirected', "pumadyn32nm", "crime", "gas",'autos', 'bike', 'keggundirected']
-
+    dataset_names = ["sonar"]
     # Main running part of the script
     for dataset_name in dataset_names:
         print(f"\nProcessing dataset: {dataset_name}")
@@ -168,29 +168,29 @@ if __name__ == '__main__':
             model_filename = f"trained_models/L2X_{dataset_name}.pkl"
             with open(model_filename, "wb") as f:
                     pickle.dump(L2X_explainer, f)
-            L2X_explainer.eval()
-            with torch.no_grad():
-                 _, l_L2X_weights= L2X_explainer(X_tensor_test, training=False)
-            L2X_weights= l_L2X_weights.cpu().numpy()
-            L2X_selected_features = (L2X_weights > 1e-3).astype(int)
-            model_filename_weights = f"importance_weights/L2X_weights_{dataset_name}.pkl"
-            with open(model_filename_weights, "wb") as f:
-                    pickle.dump(L2X_weights, f)
-            del L2X_explainer
-            memory_cleaning()
+            # L2X_explainer.eval()
+            # with torch.no_grad():
+            #      _, l_L2X_weights= L2X_explainer(X_tensor_test, training=False)
+            # L2X_weights= l_L2X_weights.cpu().numpy()
+            # L2X_selected_features = (L2X_weights > 1e-3).astype(int)
+            # model_filename_weights = f"importance_weights/L2X_weights_{dataset_name}.pkl"
+            # with open(model_filename_weights, "wb") as f:
+            #         pickle.dump(L2X_weights, f)
+            # del L2X_explainer
+            # memory_cleaning()
 
 
-            Invase_explainer = INVASE (model, X_df_train, y_series_train, n_epoch=epoch, prefit=False).to(device=device) #prefit = False to train the model
-            model_filename = f"trained_models/INVASE_{dataset_name}.pkl"
-            with open(model_filename, "wb") as f:
-                    pickle.dump(Invase_explainer, f)
-            invase_scores =(Invase_explainer.explain(X_df_test)).to_numpy()                      
-            invase_selected_features = (invase_scores > 0.5).astype(int)
-            model_filename_weights = f"importance_weights/INVASE_weights_{dataset_name}.pkl"
-            with open(model_filename_weights, "wb") as f:
-                    pickle.dump(invase_scores, f)
-            del Invase_explainer
-            memory_cleaning()
+            # Invase_explainer = INVASE (model, X_df_train, y_series_train, n_epoch=epoch, prefit=False).to(device=device) #prefit = False to train the model
+            # model_filename = f"trained_models/INVASE_{dataset_name}.pkl"
+            # with open(model_filename, "wb") as f:
+            #         pickle.dump(Invase_explainer, f)
+            # invase_scores =(Invase_explainer.explain(X_df_test)).to_numpy()                      
+            # invase_selected_features = (invase_scores > 0.5).astype(int)
+            # model_filename_weights = f"importance_weights/INVASE_weights_{dataset_name}.pkl"
+            # with open(model_filename_weights, "wb") as f:
+            #         pickle.dump(invase_scores, f)
+            # del Invase_explainer
+            # memory_cleaning()
            
 
     print("All datasets processed!")
